3f6768044e9b5537fed1806f7f0c583d484fd2e2
Chen3018 <chenjiafan3018@gmail.com>
2025-10-13 21:57:28 -0400

    Added new event card options
    added cards for logged in create card and not logged in

--- frontend/event-attendance-tracker/src/components/EventCard.tsx
+++ frontend/event-attendance-tracker/src/components/EventCard.tsx
@@ -11,11 +11,11 @@ import { Button } from "./ui/button"
 
 import { useNavigate } from "react-router-dom"
 
-export function EventCard({ text = "", event }: { text?: string, event: any }) {
+export function EventCard({ type = -1, text = "", event }: { type?: number, text?: string, event: any }) {
 
     const navigate = useNavigate();
 
-    if (text == "") {
+    if (type == 0) {
         return (
             <Card className="w-full max-w-2xs hover:scale-105 transition-transform cursor-pointer" onClick={() => navigate(`/event/${event.id}`)}>
                 <CardHeader>
@@ -34,6 +34,30 @@ export function EventCard({ text = "", event }: { text?: string, event: any }) {
                     </CardAction>
                 </CardFooter>
             </Card>    
+        )
+    } else if (type == 1) {
+        return (
+            <Card className="w-full max-w-2xs justify-center">
+
+                <CardFooter>
+                    <CardAction className="w-full">
+                        <Button className="cursor-pointer">Create New Event</Button>
+                    </CardAction>
+                </CardFooter>
+            </Card>
+        )
+    } else if (type == 2) {
+        return (
+            <Card className="w-full max-w-2xs justify-center">
+                <CardContent>{text}</CardContent>
+
+                <CardFooter>
+                    <CardAction className="w-full space-x-2">
+                        <Button variant="secondary" className="cursor-pointer">Login</Button>
+                        <Button className="cursor-pointer">Sign Up</Button>
+                    </CardAction>
+                </CardFooter>
+            </Card>
         )
     } else {
         return (
--- frontend/event-attendance-tracker/src/pages/Events.tsx
+++ frontend/event-attendance-tracker/src/pages/Events.tsx
@@ -1,26 +1,27 @@
 import { EventCard } from "@/components/EventCard"
 
 export default function Events() {
+  const isLoggedIn = false
+
   const sample = { id: 1,title: "Sample Event", date: "2023-10-01", invited: 100, checkedIn: 75 }
 
   const noCurrentText = "No ongoing event at the moment."
-  const noFutureText = "No upcoming events scheduled."
+  const loggedOutText = "Login or Sign Up to create new events."
   const noPastText = "No past events available."
-
-  const isCurrent = false
-  const isFuture = true
-  const isPast = true
 
   let currentEvent = sample
   let futureEvents = [sample, sample, sample]
   let pastEvents = [sample, sample, sample, sample]
+
+  const isCurrent = false
+  const isPast = pastEvents.length > 0
 
   return (
     <div className="max-w-[95rem] mx-auto">
       <div className="pt-8 pb-5">
         <h2 className="flex text-3xl font-semibold pb-5">Current Event</h2>
         <div className="flex gap-4 flex-wrap">
-          {isCurrent ? <EventCard event={sample} /> :
+          {isCurrent ? <EventCard type={0} event={sample} /> :
             <EventCard text={noCurrentText} event={currentEvent} />}
         </div>
       </div>
@@ -28,10 +29,11 @@ export default function Events() {
       <div className="pt-5 pb-5">
         <h2 className="flex text-3xl font-semibold pb-5">Future Events</h2>
         <div className="flex gap-4 flex-wrap">
-          {isFuture ? futureEvents.map((event, index) => (
-            <EventCard key={index} event={event} />
-          )) :
-            <EventCard text={noFutureText} event={sample} />}
+          {futureEvents.map((event, index) => (
+            <EventCard key={index} type={0} event={event} />
+          ))}
+          {isLoggedIn ? <EventCard type={1} event={sample} /> :
+            <EventCard type={2} text={loggedOutText} event={sample} />}
         </div>
       </div>
 
@@ -39,7 +41,7 @@ export default function Events() {
         <h2 className="flex text-3xl font-semibold pb-5">Past Events</h2>
         <div className="flex gap-4 flex-wrap">
           {isPast ? pastEvents.map((event, index) => (
-            <EventCard key={index} event={event} />
+            <EventCard key={index} type={0} event={event} />
           )) :
             <EventCard text={noPastText} event={sample} />}
         </div>
